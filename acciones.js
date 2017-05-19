08b559ba79ef363f13c2337af0bf27e2b220fd62
denissecazarez <deni_1407@hotmail.com>
2017-05-19 16:45:47 -0500

    vibraysuena
    vibrar y sonar

---
+++ acciones.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		$('#beep').tap(function(){
+			navigator.notification.beep(1);
+		});//Tap del beep 
+		$('#vibrar').tap(function(){
+			navigator.notification.vibrate(1000);
+		});//Tap de vibrar
+		
+	},false);//deviceready
+});//ready
